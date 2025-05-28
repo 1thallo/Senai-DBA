071eaf52b7248845ea0b60090a3227b48d6431c1
1thallo <ithalloflu@gmail.com>
2025-05-28 18:59:05 -0300

    att: Função faixa_etaria

--- Exercicios/Modelo físico - MySQL Workbench/2705-Exercício de Funções e Procedures/02.Function_faixa_etaria.sql
+++ Exercicios/Modelo físico - MySQL Workbench/2705-Exercício de Funções e Procedures/02.Function_faixa_etaria.sql
@@ -18,14 +18,20 @@ DELIMITER __
 
 	CREATE FUNCTION faixa_etaria(v_valor DATE) RETURNS VARCHAR(100)
 	NOT DETERMINISTIC
-	READS SQL DATA 
-		BEGIN
-			DECLARE v_mensagem, v_idade VARCHAR(100);
-            SET v_idade = SUBSTR(idade(v_valor), 1, 2);
-            
-            SET v_mensagem = CONCAT("A idade ", v_idade, " deve ficar entre ");
-            RETURN v_mensagem;
-		END __;
+	READS SQL DATA
+	BEGIN
+    	DECLARE v_idade, v_min, v_max INT;
+    	DECLARE v_mensagem VARCHAR(100);
+
+    	SET v_idade = idade_num(v_valor);
+    	SET v_min = (v_idade DIV 10) * 10;
+    	SET v_max = v_min + 9;
+
+    	SET v_mensagem = CONCAT('Entre ', LPAD(v_min, 2, '0'), ' e ', LPAD(v_max, 2, '0'), ' anos');
+    RETURN v_mensagem;
+END __
+
 DELIMITER ;
 
-SELECT faixa_etaria('1987-07-21') AS "Faixa Etária"; -- Deve retornar "Entre 30 e 40 anos"
+SELECT faixa_etaria('1987-07-21') AS "Faixa Etária"; -- Espero: "Entre 30 e 39 anos"
+SELECT faixa_etaria('2004-01-10') AS "Faixa Etária"; -- ''   '': "Entre 00 e 09 anos"
